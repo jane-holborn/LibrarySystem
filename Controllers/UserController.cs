bea4ab99987cfb3a8c8be6afc683100c90b23170
Jane Holborn <janeholborn@hotmail.com>
2025-04-06 11:00:00 +0800

    User management: Add setter and getter methods for users to support the borrow and return function in the Library. Add functionality to support the management of users on the Librarian dashboard.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -5,24 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using LibrarySystem.Entities;
+using static LibrarySystem.Book;
 
 namespace LibrarySystem.Controllers
 {
     public class UserController
     {
-        private List<User> ListOfAllUsers {  get; set; } = new List<User>();
+        private User currentUser;
+        private List<User> listOfAllUsers = new List<User>();
+        private List<User> listOfAllUsersWithBorrowedBooks = new List<User>();
 
         // This method is used to prepoluate the list of users when the application is launched.
         public void PrePopulateUsers()
         {
-            ListOfAllUsers.Add(new User("U644268849","James Doe", "jamesdoe@email.com"));
-            ListOfAllUsers.Add(new User("U779541212", "Charlie Evans", "charlie@email.com"));
+            listOfAllUsers.Add(new User("U295684130", "James Doe", "jamesdoe@email.com"));
+            listOfAllUsers.Add(new User("U804736591", "Charlie Evans", "charlie@email.com"));
+            listOfAllUsers.Add(new User("U612905738", "Alice Johnson", "alicejohnson@email.com"));
+            listOfAllUsers.Add(new User("U731259846", "Bob Smith", "bobsmith@email.com"));
+            listOfAllUsers.Add(new User("U854970312", "Diana Lee", "dianalee@email.com"));
+            listOfAllUsers.Add(new User("U417263890", "Ethan Brown", "ethanbrown@email.com"));
         }
 
-        // This method is used to confirm user library number
+        // This method is used to confirm user library number.
         public bool CheckUserLibraryNumber(string idNumber)
         {
-            foreach (User user in ListOfAllUsers)
+            foreach (User user in listOfAllUsers)
             {
                 if (idNumber == user.AccessToUserLibraryNumber)
                 {
@@ -30,6 +37,64 @@ namespace LibrarySystem.Controllers
                 }
             }
             return false;
+        }
+
+        // This method is used to find a user in the list of users by comparing user library numbers.
+        public User GetUserByLibraryNumber(string userLibraryNumber)
+        {
+            foreach (User user in listOfAllUsers)
+            {
+                if (userLibraryNumber == user.AccessToUserLibraryNumber)
+                {
+                    return user;
+                }
+            }
+            return null;
+        }
+
+        // This method is used to set the details of the current user.
+        public void SetCurrentUser(User user)
+        {
+            currentUser = user;
+        }
+
+        // This method is used to get the details of the current librarian.
+        public User GetCurrentUser()
+        {
+            return currentUser;
+        }
+        
+        // This method is used to check how many books a user has on loan to manage the borrow limit for each user.
+        public bool CanBorrowMoreBooks(User user)
+
+        {
+            if (user.AccessToNumberOfBorrowedBooks >= 3)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        
+        // this method is used to return a list of all the users in the library system.
+        public List<User> GetListOfAllUsers()
+        {
+            return listOfAllUsers;
+        }
+
+        // This method is update and return a list of all users with borrowed books.
+        public List<User> GetListOfAllUsersWithBorrowedBooks()
+        {
+            foreach(User user in listOfAllUsers)
+            {
+                if(user.AccessToNumberOfBorrowedBooks > 0)
+                {
+                    listOfAllUsersWithBorrowedBooks.Add(user);
+                }
+            }
+            return listOfAllUsersWithBorrowedBooks;
         }
     }
 }
--- Entities/User.cs
+++ Entities/User.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using LibrarySystem.Controllers;
+using System;
 using System.Collections.Generic;
 using System.DirectoryServices;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 using System.Windows.Media.Animation;
 
 namespace LibrarySystem.Entities
@@ -44,6 +46,20 @@ namespace LibrarySystem.Entities
         public decimal AccessToFine
         {
             get { return Fine; }
+        }
+        public List<Book>? GetBorrowedBooks()
+        {
+            return BorrowedBooks;
+        }
+
+        // Setter methods used to set update the users borrowed book list and the number of books on loan.
+        public void IncreaseNumberOfBorrowedBooks()
+        {
+            NumberOfBorrowedBooks++;
+        }
+        public void DecreaseNumberOfBorrowedBooks()
+        {
+            NumberOfBorrowedBooks--;
         }
     }
 }
